6968839c011dc13da69976e7359fc12ccd94bbd8
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2024-10-29 16:05:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import AddToDo from './compoments/AddToDo'
+import Todo from './compoments/Todo'
+
+
+
+function App() {
+
+  return (
+    <>
+      <AddToDo/>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
